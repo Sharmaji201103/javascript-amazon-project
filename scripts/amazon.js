f4b1aa86b1c10ee4de391b836947290c5f337196
Sharmaji201103 <ps4111456@gmail.com>
2026-06-06 18:03:40 +0530

    Made changes

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,3 +1,6 @@
+import {cart} from "../data/cart.js";
+import {products} from "../data/products.js"
+
 let productsHTML = '';
 
 products.forEach((product) => {
@@ -53,6 +56,7 @@ products.forEach((product) => {
     </div>`;
 
     document.querySelector('.js-products-grid').innerHTML = productsHTML;
+
     document.querySelectorAll('.js-add-to-cart').forEach((button) => {
         button.addEventListener('click', () => {
             const productId = button.dataset.productId;
@@ -74,7 +78,13 @@ products.forEach((product) => {
                     quantity: 1
                 });
             }
-            console.log(cart)
-        })
+            let cartQuantity=0;
+
+            cart.forEach((item)=>{
+                cartQuantity+=item.quantity;
+            });
+
+            document.querySelector('.js-cart-quantity').innerHTML=cartQuantity;
+        });
     })
 })
